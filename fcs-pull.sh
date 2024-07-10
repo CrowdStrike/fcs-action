5ef48edec0acce232ab8044c98cd7b5d7fc768a9
Carlos Matos <carlos.matos@crowdstrike.com>
2024-07-10 11:38:58 -0400

    debug

--- fcs-pull.sh
+++ fcs-pull.sh
@@ -9,7 +9,7 @@ set -o pipefail
 
 # Download the falcon-container-sensor-pull.sh script
 curl -O https://raw.githubusercontent.com/CrowdStrike/falcon-scripts/main/bash/containers/falcon-container-sensor-pull/falcon-container-sensor-pull.sh
-
+env
 # Check if the version is provided
 # [ -z "${INPUT_VERSION}" ] && VERSION="" || VERSION="--version ${INPUT_VERSION}"
 VERSION=${INPUT_VERSION:+"--version ${INPUT_VERSION}"}
